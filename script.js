6ead3b20b187848766d7813457bcb1c311d8dc02
JonoHasMono <fry22331@lcsd2kids.org>
2021-10-25 08:45:27 -0600

    added the css to the body variable

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const version = "0.0.1";
+const version = "0.0.2";
 
 const bodyVar = document.createElement('div');
-bodyVar.setAttribute('class', 'bodyVar');
+bodyVar.setAttribute('class','bodyVar');
 document.body.appendChild(bodyVar);
