928ea2563499168530ddf070bc6bc397bc8f0b62
JonoHasMono <fry22331@lcsd2kids.org>
2021-10-25 09:37:56 -0600

    changed the background slightly, created a start button and started working on the code that executes when the start button is clicked

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const version = "0.0.2";
+const version = "0.0.3";
 
 const bodyVar = document.createElement('div');
 bodyVar.setAttribute('class','bodyVar');
 document.body.appendChild(bodyVar);
+
+let startButton = document.createElement('div');
+startButton.setAttribute('class','startButton');
+startButton.addEventListener('click', charChoose);
+startButton.innerHTML = 'PLAY';
+bodyVar.appendChild(startButton);
+
+function charChoose() {
+    bodyVar.removeChild(startButton);
+    let charChooseText = document.createElement('div');
+}
