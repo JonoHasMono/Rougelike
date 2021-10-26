900ededc5a32fb2bcbd781db10f488d147af1d1d
JonoHasMono <fry22331@lcsd2kids.org>
2021-10-26 09:17:01 -0600

    added a bunch of new elements and animations, started working on the character cards

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "0.0.3";
+const version = "0.0.6";
 
 const bodyVar = document.createElement('div');
 bodyVar.setAttribute('class','bodyVar');
@@ -11,6 +11,45 @@ startButton.innerHTML = 'PLAY';
 bodyVar.appendChild(startButton);
 
 function charChoose() {
-    bodyVar.removeChild(startButton);
+    removeStartButton();
+    startButton.style.animation = "disappear 0.5s ease";
+    startButton.style.animationFillMode = "forwards";
+    function removeStartButton() {
+        setTimeout(() => {
+            bodyVar.removeChild(startButton);
+        },500);
+    }
     let charChooseText = document.createElement('div');
+    charChooseText.setAttribute('class', 'charChooseText');
+    charChooseText.innerHTML = 'Choose your character';
+    bodyVar.appendChild(charChooseText);
+    createDarkBG()
+    function createDarkBG() {
+        let darkBG = document.createElement('div');
+        darkBG.setAttribute('class', 'darkBG');
+        bodyVar.appendChild(darkBG);
+        darkBG.style.animation = "appear 1s ease";
+        darkBG.style.animationFillMode = "forwards";
+    }
+    moveChooseText();
+    function moveChooseText() {
+        charChooseText.style.animation = "charChooseTextAnim 1s ease";
+        charChooseText.style.animationFillMode = "forwards";
+    }
+    createCards();
+    function createCards() {
+        let jonoCard = document.createElement('div');
+        jonoCard.setAttribute('class','jonoCard');
+        bodyVar.appendChild(jonoCard);
+        let jonoCardVis = document.createElement('img');
+        jonoCardVis.setAttribute('class','jonoCardVis');
+        jonoCardVis.setAttribute('src','images/J Card.png');
+        jonoCardVis.addEventListener('click', selectJonoCard);
+        jonoCard.appendChild(jonoCardVis);
+        jonoCard.style.animation = "jonoCardAppear 1s ease";
+        jonoCard.style.animationFillMode = "forwards";
+        function selectJonoCard() {
+
+        }
+    }
 }
